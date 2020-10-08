d0fd4ecb962572ccc265c96dedaeedf764a970f4
ardhichoiruddin <ardhichoiruddin@yandex.com>
2020-10-08 20:38:16 +0700

    first commit

---
+++ content/blogPosts/apa-kau copy.md
@@ -0,0 +1,5 @@
+---
+title: Apa Kau
+date: 2019-03-17T19:31:20.591Z
+desc: halo apa kabar
+---
---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    webpack: (configuration) => {
+      configuration.module.rules.push({
+        test: /\.md$/,
+        use: 'frontmatter-markdown-loader'
+      })
+      return configuration
+    },
+  }
+  
